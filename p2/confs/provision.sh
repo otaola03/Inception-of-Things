7468091e0cf2f27e7133440c18d05aa46555deab
jperez <03.jon.perez@gmail.com>
2025-03-06 20:50:01 +0100

    💻 Starting p2, servers are working

---
+++ p2/confs/provision.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -sfL https://get.k3s.io | sh -
+# sudo rc-update add k3s default
+# sudo rc-service k3s start
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+
+
