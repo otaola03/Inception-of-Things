5b8d0194ba5e095de09796c1a724aeadfd89c940
jperez <03.jon.perez@gmail.com>
2025-03-07 16:36:12 +0100

    😴 Longer sleep in p1

--- p1/confs/server.sh
+++ p1/confs/server.sh
@@ -4,12 +4,13 @@ sudo curl -sfL https://get.k3s.io | sh -
 # sudo rc-update add k3s default
 # sudo rc-service k3s start
 
+sleep 10  # Espera 5 segundos antes de verificar nuevamente
+
 while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
   echo "Esperando a que se cree el archivo node-token..."
   sleep 5  # Espera 5 segundos antes de verificar nuevamente
 done
 
-sleep 10  # Espera 5 segundos antes de verificar nuevamente
 
 cp /var/lib/rancher/k3s/server/node-token /shared/node-token
 
