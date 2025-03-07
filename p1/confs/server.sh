237fc56d6ab6c67e5b0046f11d3cb712884983be
jperez <03.jon.perez@gmail.com>
2025-03-07 16:10:20 +0100

    🪄 P1 is working

--- p1/confs/server.sh
+++ p1/confs/server.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-curl -sfL https://get.k3s.io | sh -
-sudo rc-update add k3s default
-sudo rc-service k3s start
+sudo curl -sfL https://get.k3s.io | sh -
+# sudo rc-update add k3s default
+# sudo rc-service k3s start
 
 while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
   echo "Esperando a que se cree el archivo node-token..."
   sleep 5  # Espera 5 segundos antes de verificar nuevamente
 done
 
+sleep 10  # Espera 5 segundos antes de verificar nuevamente
+
 cp /var/lib/rancher/k3s/server/node-token /shared/node-token
+
+echo "==================TOKEN==========================="
+cat /shared/node-token
+echo "=================================================="
 
 echo "El archivo node-token se ha movido a /shared."
 
--- p1/confs/worker.sh
+++ p1/confs/worker.sh
@@ -2,4 +2,7 @@
 
 TOKEN=$(cat /shared/node-token)
 
-curl -sfL https://get.k3s.io | K3S_URL=https://$1:6443 K3S_TOKEN="$TOKEN" sh -
+echo "curl -sfL https://get.k3s.io | K3S_URL=https://$1:6443 K3S_TOKEN=\"$TOKEN\" sh -s - agent --node-name=worker-01"
+
+curl -sfL https://get.k3s.io | K3S_URL=https://$1:6443 K3S_TOKEN="$TOKEN" sh -s - agent --node-name=worker-01
+
