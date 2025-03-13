c08ca21b4a8ffa3b8f4a021c8c2a306a9f86a9f8
jperez <03.jon.perez@gmail.com>
2025-03-13 10:58:19 +0100

    🕸 Installed K3D and kubectl

--- p3/confs/init.sh
+++ p3/confs/init.sh
@@ -14,3 +14,19 @@ sudo apt-get update
 
 # Install Docker Engine:
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install K3D:
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# Install Kubectl:
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+if echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check | grep -q 'OK'; then
+    echo "La verificación fue exitosa."
+else
+    echo "La verificación falló."
+	return 1
+fi
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
