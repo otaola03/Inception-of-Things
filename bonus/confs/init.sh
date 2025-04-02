7a3fbc4c361b048384fda295594c934f202d9703
jperez <03.jon.perez@gmail.com>
2025-04-02 17:37:50 +0200

    📡 Ingress working in bonus

--- bonus/confs/init.sh
+++ bonus/confs/init.sh
@@ -1,4 +1,13 @@
+
+red='\033[31m'
+green='\033[32m'
+blue='\033[34m'
+gold='\033[1m'
+underline='\033[4m'
+reset='\033[0m'
+
 # Add Docker's official GPG key:
+echo "${blue}🐋 Insatlling Docker${reset}"
 sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -15,16 +24,20 @@ sudo apt-get update
 # Install Docker Engine:
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+
 # Install K3D:
+echo "${blue}🐋 Insatlling K3D${reset}"
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
+
+echo "${blue}🐋 Insatlling Kubectl${reset}"
 # Install Kubectl:
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 if echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check | grep -q 'OK'; then
-    echo "La verificación fue exitosa."
+    echo ${green}"La verificación fue exitosa.${reset}"
 else
-    echo "La verificación falló."
+    echo ${red}"La verificación falló.${reset}"
 	return 1
 fi
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
@@ -32,31 +45,60 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 
 # Create a K3D cluster:
+echo "${blue}📦 Creating k3d cluster${reset}"
 # sudo k3d cluster create my-cluster
 # sudo k3d cluster create argocd-cluster --api-port 6550 --port 8080:80 --port 8443:443
 # sudo k3d cluster create mycluster --api-port 6443 --port 8080:80@loadbalancer --port 8443:443@loadbalancer
 # sudo k3d cluster create mycluster -p "8080:80@loadbalancer"
-sudo k3d cluster create mycluster --servers 1 --agents 2 -p "8081:80@loadbalancer"
+sudo k3d cluster create mycluster -p "8082:80@loadbalancer"
+
+
+# Install Ingress Nginx:
+echo "${blue}📡 Installing Ingress Nginx${reset}"
+sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml --validate=false
+
+echo "${blue}⏳ Waiting for ingress-nginx-controller pod to be ready...${reset}"
+while true; do
+    # Obtener la línea correspondiente al pod ingress-nginx-controller
+    status_line=$(sudo kubectl get pods -n ingress-nginx | grep "ingress-nginx-controller")
+    
+    # Obtener el estado de 'READY' y 'STATUS'
+    ready_status=$(echo "$status_line" | awk '{print $2}')
+    pod_status=$(echo "$status_line" | awk '{print $3}')
+    
+	echo "line: $status_line"
+    # Verificar si el pod está listo y en ejecución
+    if [[ "$ready_status" == "1/1" && "$pod_status" == "Running" ]]; then
+        echo "${green}El pod ingress-nginx-controller está listo y en ejecución.${reset}"
+        break  # Salir del bucle una vez que la condición se cumpla
+    fi
+    
+    # Si no está listo, esperar 5 segundos y volver a comprobar
+    sleep 2
+done
 
 
 # Install ArgoCD:
+echo "${blue}📦 Installing ArgoCD${reset}"
 sudo kubectl create namespace argocd
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# Install Ingress Nginx:
-sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml --validate=false
 
 # Install Helm:
+echo "${blue}📦 Installing Helm${reset}"
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 # Add gitlab Helm repository:
+echo "${blue}🦊 Adding Gitlab Helm repository${reset}"
 sudo helm repo add gitlab https://charts.gitlab.io
 sudo helm repo update
 
 # Install Gitlab:
+echo "${blue}🦊 Installing Gitlab${reset}"
 sudo kubectl create namespace gitlab
 sudo helm install gitlab gitlab/gitlab -n gitlab -f /shared/values.yaml
 
 
 # Create dev namespace:t
+echo "${blue}📛 Creating dev namespace${reset}"
 sudo kubectl create namespace dev
