4c91340944562acb75f87d158ccdb9761a745823
jperez <03.jon.perez@gmail.com>
2025-04-03 17:46:06 +0200

    🦋 Preattier p1

--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+sleep 10
+
 TOKEN=$(cat /shared/node-token)
 
 echo "curl -sfL https://get.k3s.io | K3S_URL=https://$1:6443 K3S_TOKEN=\"$TOKEN\" sh -s - agent --node-name=worker-01"
 
-curl -sfL https://get.k3s.io | K3S_URL=https://$1:6443 K3S_TOKEN="$TOKEN" sh -s - agent --node-name=worker-01
+curl -sfL https://get.k3s.io | K3S_URL=https://$1:6443 K3S_TOKEN=$TOKEN sh -s - agent --node-name=worker-01
 
